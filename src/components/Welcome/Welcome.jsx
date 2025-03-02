12afeb16ff1428f19e5e052e888e620f616b7426
Yevheniia-narizhna <162164893+Yevheniia-narizhna@users.noreply.github.com>
2025-03-02 16:29:56 +0200

    fonts-cssWelcome

--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -4,9 +4,9 @@ const Welcome = () => {
   return (
     <div className={s.welcomeCont}>
       <div className={s.contText}>
-        <h1>Make Life Easier for the Family:</h1>
-        <p>Find Babysitters Online for All Occasions</p>
-        <button>Get started</button>
+        <h1 className={s.title}>Make Life Easier for the Family:</h1>
+        <p className={s.textWelc}>Find Babysitters Online for All Occasions</p>
+        <button className={s.btnWelc}>Get started</button>
       </div>
       <div className={s.contImg}></div>
     </div>
