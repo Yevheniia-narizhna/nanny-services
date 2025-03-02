50a1c97ab93bdb0a357d8a291ce1996a92602186
Yevheniia-narizhna <162164893+Yevheniia-narizhna@users.noreply.github.com>
2025-03-02 20:57:36 +0200

    active-links

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,19 @@
 import { NavLink, useLocation } from "react-router-dom";
 import s from "./Header.module.css";
+import { useState } from "react";
+import Login from "../Login/Login";
+import Registration from "../Registration/Registration";
 
 const Header = () => {
   const location = useLocation();
+  const [activeButton, setActiveButton] = useState("register");
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleClick = (buttonType) => {
+    setActiveButton(buttonType);
+    setIsModalOpen(true);
+  };
+
   return (
     <div className={s.headerCont}>
       <img className={s.iconLogo} src="/logo-nanny.svg" alt="Logo" />
@@ -29,6 +40,34 @@ const Header = () => {
           </NavLink>
         )}
       </div>
+      <div className={s.btnCont}>
+        <button
+          type="button"
+          className={`${s.btnLog} ${
+            activeButton === "login" ? s.activeBtn : ""
+          }`}
+          onClick={() => handleClick("login")}
+        >
+          Log in
+        </button>
+        <button
+          type="button"
+          className={`${s.btnReg} ${
+            activeButton === "register" ? s.activeBtn : ""
+          }`}
+          onClick={() => handleClick("register")}
+        >
+          Registration
+        </button>
+      </div>
+      {isModalOpen && (
+        <div className={s.modalOverlay} onClick={() => setIsModalOpen(false)}>
+          <div className={s.modalContent} onClick={(e) => e.stopPropagation()}>
+            <h2>{activeButton === "login" ? <Login /> : <Registration />}</h2>
+            <button onClick={() => setIsModalOpen(false)}>Закрити</button>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
