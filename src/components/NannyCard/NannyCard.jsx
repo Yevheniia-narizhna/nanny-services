d61ae73de31164709f45f83b63cca8d9962f4286
Yevheniia-narizhna <162164893+Yevheniia-narizhna@users.noreply.github.com>
2025-03-12 22:09:13 +0200

    card-next-css

--- src/components/NannyCard/NannyCard.jsx
+++ src/components/NannyCard/NannyCard.jsx
@@ -6,6 +6,11 @@ import { GoStarFill } from "react-icons/go";
 
 const NannyCard = ({ nanny }) => {
   const [age, setAge] = useState(null);
+  const [showReviews, setShowReviews] = useState(false);
+
+  const handleClick = () => {
+    setShowReviews(true);
+  };
 
   useEffect(() => {
     if (nanny.birthday) {
@@ -15,37 +20,56 @@ const NannyCard = ({ nanny }) => {
 
   return (
     <div className={s.card}>
-      <img src={nanny.avatar_url} alt={nanny.name} className={s.cardImg} />
-      <p>Nanny</p>
-      <ul>
-        <li>
-          <svg className={s.svgLoc}>
-            <use href="/symbol-defs.svg#icon-map-pin"></use>
+      <div>
+        <img src={nanny.avatar_url} alt={nanny.name} className={s.cardImg} />
+      </div>
+      <div>
+        <p>Nanny</p>
+        <ul className={s.ulLoc}>
+          <li className={s.liLoc}>
+            <svg className={s.svgLoc}>
+              <use href="/symbol-defs.svg#icon-map-pin"></use>
+            </svg>
+            <span>{nanny.location}</span>
+          </li>
+          <li className={s.liRat}>
+            <GoStarFill className={s.svgRat} />
+            <span>Rating: {nanny.rating}</span>
+          </li>
+          <li className={s.liPr}>
+            <span>Price / 1 hour: ${nanny.price_per_hour}</span>
+          </li>
+        </ul>
+        <button className={s.btnFav}>
+          <svg className={s.svgFav}>
+            <use href="/symbol-defs.svg#icon-Property-1Normal"></use>
           </svg>
-          <span>{nanny.location}</span>
-        </li>
-        <li>
-          <GoStarFill className={s.svgRat} />
-          <span>Rating: {nanny.rating}</span>
-        </li>
-        <li>
-          <span>Price / 1 hour: ${nanny.price_per_hour}</span>
-        </li>
-      </ul>
-      <h3>{nanny.name}</h3>
-      <ul>
-        <li>Age: {age}</li>
-        <li>Experience: {nanny.experience}</li>
-        <li>Kids Age: {nanny.kids_age}</li>
-        <li>
-          Characters:{" "}
-          {nanny.characters
-            .map((char) => char.charAt(0).toUpperCase() + char.slice(1))
-            .join(", ")}
-        </li>
-        <li>Education: {nanny.education}</li>
-      </ul>
-      <p>{nanny.about}</p>
+        </button>
+        <h3>{nanny.name}</h3>
+        <ul>
+          <li>Age: {age}</li>
+          <li>Experience: {nanny.experience}</li>
+          <li>Kids Age: {nanny.kids_age}</li>
+          <li>
+            Characters:{" "}
+            {nanny.characters
+              .map((char) => char.charAt(0).toUpperCase() + char.slice(1))
+              .join(", ")}
+          </li>
+          <li>Education: {nanny.education}</li>
+        </ul>
+        <p>{nanny.about}</p>
+        {!showReviews && (
+          <button onClick={handleClick}>Read more</button> // Кнопка "Read more"
+        )}
+        {showReviews && (
+          <div className="reviews">
+            <p>Review 1</p>
+            <p>Review 2</p>
+            <button>Make an appointment</button>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
