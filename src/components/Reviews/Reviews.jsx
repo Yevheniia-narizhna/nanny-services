d00cade53fa1c934b17607f7628c399a1162d9ea
Yevheniia-narizhna <162164893+Yevheniia-narizhna@users.noreply.github.com>
2025-03-13 17:05:01 +0200

    reviews-css

--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -1,19 +1,31 @@
+import { GoStarFill } from "react-icons/go";
+import s from "./Reviews.module.css";
+
 const Reviews = ({ reviews }) => {
   if (!reviews || reviews.length === 0) {
     return <p>No reviews available</p>;
   }
 
   return (
-    <div className="reviews">
-      <h3>Reviews</h3>
-      {reviews.map((review, index) => (
-        <div key={index}>
-          <p>
-            <strong>{review.reviewer}:</strong> {review.comment}
-          </p>
-          <p>Rating: {review.rating}</p>
-        </div>
-      ))}
+    <div>
+      <div className={s.contRev}>
+        {reviews.map((review, index) => (
+          <div key={index}>
+            <div className={s.titles}>
+              <p className={s.letter}>{review.reviewer.charAt(0)}</p>
+              <div className={s.nameStar}>
+                <p>{review.reviewer}</p>
+                <div className={s.contStar}>
+                  <GoStarFill className={s.iconStar} />
+                  <span>{Number(review.rating).toFixed(1)}</span>
+                </div>
+              </div>
+            </div>
+            <p className={s.textRew}>{review.comment}</p>
+          </div>
+        ))}
+      </div>
+      <button className={s.btnRew}>Make an appointment</button>
     </div>
   );
 };
